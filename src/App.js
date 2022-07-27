1db7ede8e73d3f75b4461bce18e0843b2366b848
wafiben <wafibenjeddou@gmail.com>
2022-07-27 21:25:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+class App extends Component {
+  state = { show: false,count:0 };
+  handleClick = () => {
+    this.state.show
+      ? this.setState({ show: false })
+      : this.setState({ show: true });
+  };
+
+  increment=()=>{
+    this.setState({count: this.state.count+1})
+  }
+  decrement=()=>{
+    this.state.count>0 &&  this.setState({count: this.state.count-1}) 
+       
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button
+          onClick={this.handleClick}
+          /* className={this.state.show ? "btnTrue" : "btnFlse"} */
+          style={this.state.show ? {backgroundColor:"green"}: {backgroundColor:"red"}}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          changeMyStyle
+        </button>
+        <div>
+        <button onClick={this.increment}>+</button>
+        <h3>{this.state.count}</h3>
+        <button onClick={this.decrement}>-</button>
+        </div>
 
+      </div>
+    );
+  }
+}
 export default App;
